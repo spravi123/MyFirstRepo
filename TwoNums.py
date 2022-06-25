1bd00b92af20c79d960096f06e1ef3f8aef3a06f
sprav123 <sprav123>
2022-06-25 21:28:19 +0530

    Committing

---
+++ TwoNums.py
@@ -0,0 +1,10 @@
+# This program multiply two numbers
+
+num1 = 1.5
+num2 = 6.3
+
+# Multiply two numbers
+sum = num1 * num2
+
+# Display the Result
+print('The multiplication of {0} and {1} is {2}'.format(num1, num2, sum))
